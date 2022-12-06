f18366aefca2eca4cd3ba7022272bd3debea92f6
[Norah Taylor] <emt368@gmail.com>
2022-12-06 15:56:32 -0500

    commit tree 16ef51f09d7edf9c28fd1422a5b24139d3f02b5f

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(num1,num2){
+   return num1/num2
+}
+
+const square = (num) => num * num 
+
+const add = (a,b) => a + b
+
+// function divide(num1, num2) {
+//     return 2000 / 100
+// }
+// //divide(2000,100)
+
+// const square = (num3) => num3 * num3
+
+// const add = (num4, num5) => num4 + num5
